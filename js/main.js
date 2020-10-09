83490e7f882d912db28e6f32ecb414291c0fc9dc
DesmondKwao <72413189+DesmondKwao@users.noreply.github.com>
2020-10-09 23:04:07 +0000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,40 @@
 $(document).ready(function(){
+$('#create-card').on('click',function(){
+  let name= $('#new-card').val(); // accept new input
+  $('#new-card').val(''); //to clear input after entry
+
+  let html = '';
+    html += '<div class="shop">';
+    html += '<div class="name">'+ name +'</div>';
+    html += '<img src="assets/ghana.jpg"></img>';
+    html += '<div class="description"> Spanning a land mass of 238,535 km2 (92,099 sq mi),Ghana is bordered by the Ivory Coast in the west,Burkina Faso in the north, Togo in the east,</div>';
+    html += '<br/>';
+    html += '<div class="rate">Rate: $499.00</div>';
+    html += '<br/>';
+    html += '<button class="cart-add" type="button" name="button">Add to Cart</button>';
+    html += '<button class="cart-remove" type="button" name="button">Remove from Cart</button>';
+    html += '<br/>';
+    html += '<a class="more-info-link" href="#">More info</a>';
+    html += '<div class="more-info">and the Gulf of Guinea and Atlantic Ocean in the south.Ghana means "Warrior King" in the Soninke language.</div>';
+    html += '</div>';
+
+  $('#design').prepend(html); //add a new cart
+});
+
+$('#design').on('click','.more-info-link',function(event){
+ event.preventDefault();
+  $(this).parent().find('.more-info').toggle(100);//this display the more info hidden
+ $(this)
+ .animate({"margin-left": 5}, 'fast')
+ .animate({"margin-left": 0}, 'fast');
+
+});
+$('#design').on('click','.cart-remove',function(){
+  $(this).parent().remove(); // remove cart from entry
+
+});
+
+
 $('#newsletter-checkbox').on('change',function(){
   if($(this).is(':checked')){
   $('#newsletter-select').show();
@@ -18,7 +54,8 @@ let data=  { form: $(this).serialze() };
  })
  .done(function(response){
    $('feedback').text(response.message)
-   alert(data.form + ' : ' + response.message);
- });
+
+
+});
 });
 });
